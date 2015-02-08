4f39e54c5e942df914aacdc318b653327a920ab2
lgh06 <hnnk@qq.com>
2015-02-08 14:57:26 +0800

    del unused page

--- src/main/java/com/lgh/sys/entity/Department.java
+++ src/main/java/com/lgh/sys/entity/Department.java
@@ -2,13 +2,8 @@ package com.lgh.sys.entity;
 
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import com.lgh.common.entity.BaseEntity;
 
@@ -21,7 +16,7 @@ public class Department extends BaseEntity implements Serializable {
 	private static final long serialVersionUID = 8577494607947632638L;
 	
 	private String name;
-	private Set<Admin> admins = new HashSet<Admin>(0);
+	/*private Set<Admin> admins = new HashSet<Admin>(0);*/
 
 
 	@Column(name = "name", length = 32)
@@ -33,13 +28,13 @@ public class Department extends BaseEntity implements Serializable {
 		this.name = name;
 	}
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "department")
+	/*@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "department")
 	public Set<Admin> getAdmins() {
 		return this.admins;
 	}
 
 	public void setAdmins(Set<Admin> admins) {
 		this.admins = admins;
-	}
+	}*/
 
 }
